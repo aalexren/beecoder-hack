29abcfe6ca2a5966684775783ec08c00af105bf7
Rodion <chernitca@ya.ru>
2022-04-19 02:11:56 +0300

    qr-code entrance

--- clock.py
+++ clock.py
@@ -8,9 +8,12 @@ from datetime import datetime
 
 @sched.scheduled_job('interval', seconds=5)
 def timed_job():
+    f = open("app/static/qr_file/qr_file.txt", "r")
+    # seed(datetime.now())
+    qr_num = f.read()
+    f.close()
     f = open("app/static/qr_file/qr_file.txt", "w")
-    seed(datetime.now())
-    qr_num = randint(1, 100000000)
+    qr_num = (int(qr_num) + 17) % 100000000
     f.write(str(qr_num))
     f.close()
 
