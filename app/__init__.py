f253db91d9a112909240eb3716efecfbc594876b
Rodion <chernitca@ya.ru>
2022-04-19 15:08:50 +0300

    smart devices and sensors

--- app/__init__.py
+++ app/__init__.py
@@ -35,7 +35,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate('beecoder.json')
+cred = credentials.Certificate('../beecoder.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
