25251c72207ff24ce484ed048c3a0b214368324f
Rodion <chernitca@ya.ru>
2022-04-19 13:27:34 +0300

    smart devices and sensors

--- app/sim_sensors.py
+++ app/sim_sensors.py
@@ -1,30 +1,41 @@
 from time import time, sleep
-import math
-
+import random
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-def mock_value():
-    dt = time()
-    res = abs(math.sin(dt) * 10) + 18
-    return res
+
+def mock_temperature(prev):
+    return prev + random.randrange(-30, 30) / 100
+
+
+def mock_humidity(prev):
+    return prev + random.randrange(-25, 25) / 100
+
+
+mock = {
+    'temperature_sensor': mock_temperature,
+    'humidity_sensor': mock_humidity      
+}
+
 
 def main():
     # Simulate sensors
-
     cred = credentials.Certificate('../beecoder.json')
     firebase_admin.initialize_app(cred)
 
     db = firestore.client()
 
     while True:
-        sleep(5)
+        sleep(1)
         coll = db.collection('sensors')
         devs = coll.stream()
         for dev in devs:
             ref = coll.document(dev.id)
-            ref.update({'value':mock_value()})
+            dev = dev.to_dict()
+            ref.update({'value': mock[dev['type']](dev['value'])})
+            
+            
 
 if __name__ == '__main__':
     main()
